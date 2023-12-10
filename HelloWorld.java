c6aa66c1f9564e3705ce0e42f51e6b3cef11ec29
niceycg <niceycg@gmail.com>
2023-12-10 23:17:15 +0900

    고의젝 에러 발생
    코드리뷰 테스트중

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        if ( 1024 / 0 == 0 ) {
+            System.out.println("1024를 0으로 나누면?");
+        }
     }
 }
