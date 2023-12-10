a5fe3d73def7a54c019720017d6a8384e85332ff
niceycg <niceycg@gmail.com>
2023-12-10 22:11:04 +0900

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        if( "abe" != 0 ) {
+            // 에러 발생 테스트 
+            
+        }
     }
 }
